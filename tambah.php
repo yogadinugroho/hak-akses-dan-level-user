c273af246d07bf0bfddd4815931ba0093517fc21
Yoga Adi Nugroho <78912216+yogadinugroho@users.noreply.github.com>
2021-06-26 19:00:43 +0700

    mengaktifkan logic tambah data
    menambahkan logic PHP agar button tambah data berfungsi

--- tambah.php
+++ tambah.php
@@ -1,3 +1,46 @@
+<?php 
+
+session_start();
+
+// jika tidak ada session login, maka pindahkan ke login.php
+if(!isset($_SESSION["login"])) {
+    header("Location: login.php");
+    exit;
+}
+
+require 'functions.php';
+
+// koneksi ke database
+$conn = mysqli_connect("localhost", "root", "", "phpdasar");
+
+// cek apakah tombol submit sudah di tekan atau belum
+if( isset($_POST["submit"]) ) {
+
+    // untuk debugging 
+    // var_dump($_POST); die;
+
+    // cek apakah data berhasil ditambahkan atau tidak
+    if( tambah($_POST) > 0 ) {
+        echo "
+            <script>
+                alert('data berhasil ditambahkan!');
+                document.location.href = 'index.php';
+            </script>
+        ";
+    } else {
+        echo "
+            <script>
+            alert('data gagal ditambahkan!');
+            document.location.href = 'index.php';
+            </script>
+        ";
+    }
+
+}
+
+
+?>
+
 
 <!DOCTYPE html>
 <html lang="en">
