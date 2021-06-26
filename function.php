2a6e68375a7f70d25a6c9ae175da08e0b1d45c78
Yoga Adi Nugroho <78912216+yogadinugroho@users.noreply.github.com>
2021-06-26 18:58:00 +0700

    menambahkan function tambah

--- function.php
+++ function.php
@@ -14,6 +14,34 @@ function query($query) {
         $rows[] = $row;
     }
     return $rows;
+    
+}
 
+function tambah($data) {
+    global $conn;
+
+    // gambar diupload ke dir kemudian ambil nama gambar
+    // baru nama gambarnya kita insert ke dalam database
+    
+    // upload gambar
+    $gambar = upload();
+    if ( !$gambar ) {
+        return false;
+    }
+    
+    // ambil data dari tiap elemen dalam form
+    $nrp = htmlspecialchars($data["nrp"]);
+    $nama = htmlspecialchars($data["nama"]);
+    $email = htmlspecialchars($data["email"]);
+    $jurusan =htmlspecialchars($data["jurusan"]);
+
+    // query untuk memasukkan data ke database
+    $query = "INSERT INTO mahasiswa VALUES 
+            ('', '$nrp', '$nama', '$email', '$jurusan', '$gambar')";
+
+    mysqli_query($conn, $query);    
+
+    return mysqli_affected_rows($conn);
+}
 
 ?>
