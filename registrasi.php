e516717542b6d28a79343adae783d3d2cd86c2ca
Yoga Adi Nugroho <78912216+yogadinugroho@users.noreply.github.com>
2021-06-26 19:25:33 +0700

    membuat form html registrasi.php

---
+++ registrasi.php
@@ -0,0 +1,44 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Halaman Registrasi</title>
+    <style>
+        label {
+            display: block;
+        }
+    </style>
+</head>
+<body>
+    <h1>Halaman Registrasi</h1>
+
+    <form action="" method="POST">
+    
+    <ul>
+        <li>
+            <label for="username">Username :</label>
+            <input type="text" name="username" id="username">
+        </li>
+
+        <li>
+            <label for="password">Password :</label>
+            <input type="password" name="password" id="password">
+        </li>
+        
+        <li>
+            <label for="password2">Konfirmasi password :</label>
+            <input type="password" name="password2" id="password2">
+        </li>
+    
+        <br>
+        <li>
+            <button name="register" type="submit">Register!</button>
+        </li>
+    
+    </ul>
+    
+    
+    </form>
+</body>
+</html>
