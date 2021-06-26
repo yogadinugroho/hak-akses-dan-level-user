61bc2eca4ac2344f148fee6d6358b1a831a8e4a9
Yoga Adi Nugroho <78912216+yogadinugroho@users.noreply.github.com>
2021-06-26 19:20:39 +0700

    menambahkan function hapus

--- functions.php
+++ functions.php
@@ -102,4 +102,10 @@ function upload() {
 }
 
 
+function hapus($id) {
+    global $conn;
+    mysqli_query($conn, "DELETE FROM mahasiswa WHERE id = $id ");
+    return mysqli_affected_rows($conn);
+}
+
 ?>
