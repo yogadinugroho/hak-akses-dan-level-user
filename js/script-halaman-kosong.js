86801f88d07395f1499903998abc7b59b5651704
Yoga Adi Nugroho <78912216+yogadinugroho@users.noreply.github.com>
2021-06-27 09:08:53 +0700

    membuat folder dan script halaman 0
    membuat folder yang menampung file js. digunakan untuk javascript livesearch dimana user yang mempunyai hak akses penuh atau admin.

---
+++ js/script-halaman-kosong.js
@@ -0,0 +1,40 @@
+// bisa diganti dengan
+// jQuery(document)
+
+// jquery tolong ambilkan document, lalu jika
+// document siap, jalankan function
+
+$(document).ready(function(){
+
+    // menghilangkan tombol cari
+    $('#tombol-cari').hide();
+
+    // event ketika keyword ditulis
+    // jquery tolong carikan elemen keyword
+    // ketika keyup lakukan function berikut ini
+
+    $('#keyword').on('keyup', function() {
+
+        // munculkan gif loading
+        $('.loader').show();
+
+        // jquery tolong carikan saya elemen container
+        // lalu load atau ubah isinya dengan data yang kita ambil dari sumber
+        // apapun yang diketikkan user di keyword
+        
+        // ajax menggunakan load
+        // $('#container').load('ajax/mahasiswa.php?keyword=' + $('#keyword').val());
+
+        // $.get
+        // jquery lakukan get ke sumber
+        $.get('ajax/mahasiswa-halaman-kosong.php?keyword=' + $('#keyword').val(), function(data) {
+
+            $('#container').html(data);
+            $('.loader').hide();
+
+        });
+
+    });
+
+
+});
